518da3445d42d1461654431c4629f4aa3a972839
riaz9191 <riazahammed9191@gmail.com>
2025-08-09 21:52:58 +0600

    Refactor book fetching logic to include filtering, sorting, and pagination

--- src/app/controller/book.controller.ts
+++ src/app/controller/book.controller.ts
@@ -3,53 +3,70 @@ import Book from "../model/book.model";
 export const bookRoutes = express.Router();
 
 bookRoutes.get("/", async (req: Request, res: Response) => {
-    const body = req.body;
+  const body = req.body;
+  const {
+    filter,
+    sortBy = "createdAt",
+    sort = "asc",
+    limit = 10,
+    skip,
+  } = req.query;
 
-    const books = await Book.find(body);
-    res.status(200).json({
-        success: true,
-        message: "Books fetched successfully",
-        data: books,
-    });
+  const query: any = {};
+  if (filter) {
+    query.genre = filter;
+  }
+
+//   const books = await Book.find(body);
+
+    const books = await Book.find(query)
+      .sort({ [sortBy as string]: sort === 'desc' ? -1 : 1 })
+      .limit(parseInt(limit as string, 10)); 
+      
+      res.status(200).json({
+    success: true,
+    message: "Books fetched successfully",
+    data: books,
+  });
 });
 
 bookRoutes.post("/create-books", async (req: Request, res: Response) => {
-    const body = req.body;
-    const books = await Book.create(body);
-    res.status(200).json({
-        success: true,
-        message: "Books created successfully",
-        data: books,
-    });
+  const body = req.body;
+  const books = await Book.create(body);
+  res.status(200).json({
+    success: true,
+    message: "Books created successfully",
+    data: books,
+  });
 });
 
 bookRoutes.get("/:id", async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const book = await Book.findById(id);
-    res.status(200).json({
-        success: true,
-        message: "Book fetched successfully",
-        data: book,
-    });
+  const id = req.params.id;
+  const book = await Book.findById(id);
+  res.status(200).json({
+    success: true,
+    message: "Book fetched successfully",
+    data: book,
+  });
 });
 
 bookRoutes.patch("/:id", async (req: Request, res: Response) => {
-   const id = req.params.id;
-   const body = req.body;
-   const book = await Book.findByIdAndUpdate(id, body, { new: true });
-   res.status(200).json({
-       success: true,
-       message: "Book updated successfully",
-       data: book,
-   });
+  const id = req.params.id;
+  const body = req.body;
+  const book = await Book.findByIdAndUpdate(id, body, { new: true });
+  res.status(200).json({
+    success: true,
+    message: "Book updated successfully",
+    data: book,
+  });
 });
 
 bookRoutes.delete("/:id", async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const book = await Book.findByIdAndDelete(id);
-    res.status(200).json({
-        success: true,
-        message: "Book deleted successfully",
-        data: book,
-    });
+  const id = req.params.id;
+  const book = await Book.findByIdAndDelete(id);
+  res.status(200).json({
+    success: true,
+    message: "Book deleted successfully",
+    data: book,
+  });
 });
